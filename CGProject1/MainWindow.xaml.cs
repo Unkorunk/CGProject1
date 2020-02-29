c6804c4e5c16cf133a0b4cc7025c9eb935298ec1
Unkorunk <unkorunk@gmail.com>
2020-02-29 10:30:02 +1000

    Add project files.

---
+++ CGProject1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CGProject1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("КГ-СИСТПРО-1-КАЛИНИН\r\n" +
+                "Работу выполнили:\r\n" +
+                "Михалев Юрий\r\n" +
+                "Калинин Владислав\r\n" +
+                "29.02.2020",
+                "О программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
