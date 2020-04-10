4950ea113cf9b43ac0f9d6518f93b182505ad325
Unkorunk <unkorunk@gmail.com>
2020-04-10 16:37:04 +1000

    Add optimization chart draw

--- CGProject1/Chart/Chart.cs
+++ CGProject1/Chart/Chart.cs
@@ -39,6 +39,14 @@ namespace CGProject1
         protected override void OnRender(DrawingContext dc)
         {
             double stepX = this.ActualWidth / this.Length;
+            bool optimization = (stepX < 0.5);
+            int stepOptimization = 0;
+            if (optimization)
+            {
+                stepOptimization = (int)Math.Ceiling(0.5 * this.Length / this.ActualWidth);
+                stepX *= stepOptimization;
+            }
+
             double stepY = this.ActualHeight;
 
             double offsetY = this.ActualHeight / 2.0 - stepY / 2.0;
@@ -50,13 +58,69 @@ namespace CGProject1
 
             dc.DrawRectangle(this.Selected ? Brushes.LightBlue : Brushes.LightGray,
                 new Pen(Brushes.DarkGray, 2.0), new Rect(0, 0, ActualWidth, ActualHeight));
-            for (int i = this.Begin + 1; i <= this.End; i++)
+
+            if (optimization)
             {
+                double prevValueMin = double.MaxValue;
+                double prevValueMax = double.MinValue;
+                for (int i = 0; i < stepOptimization && this.Begin + i < this.channel.values.Length; i++)
+                {
+                    prevValueMin = Math.Min(prevValueMin, this.channel.values[this.Begin + i]);
+                    prevValueMax = Math.Max(prevValueMax, this.channel.values[this.Begin + i]);
+                }
                 dc.DrawLine(
                     new Pen(Brushes.Black, 1.0),
-                    new Point((i - this.Begin - 1) * stepX, stepY * (this.channel.values[i - 1] - channelMinValue) / height + offsetY),
-                    new Point((i - this.Begin) * stepX, stepY * (this.channel.values[i] - channelMinValue) / height + offsetY)
+                    new Point(0, stepY * (prevValueMin - channelMinValue) / height + offsetY),
+                    new Point(stepX / 2.0, stepY * (prevValueMax - channelMinValue) / height + offsetY)
                 );
+                int lineX = 1;
+                int valuesX = 1;
+
+                for (int i = 1; i < this.Length; i += stepOptimization)
+                {
+                    double nowValueMin = double.MaxValue;
+                    double nowValueMax = double.MinValue;
+                    for (int j = 0; j < stepOptimization && this.Begin + valuesX * stepOptimization + j < this.channel.values.Length; j++)
+                    {
+                        nowValueMin = Math.Min(nowValueMin, this.channel.values[this.Begin + valuesX * stepOptimization + j]);
+                        nowValueMax = Math.Max(nowValueMax, this.channel.values[this.Begin + valuesX * stepOptimization + j]);
+                    }
+                    valuesX++;
+                    if (Math.Abs(nowValueMax - prevValueMax) < Math.Abs(nowValueMin - prevValueMax))
+                    {
+                        double t = nowValueMax;
+                        nowValueMax = nowValueMin;
+                        nowValueMin = t;
+                    }
+
+                    dc.DrawLine(
+                        new Pen(Brushes.Black, 1.0),
+                        new Point(lineX * stepX / 2.0, stepY * (prevValueMax - channelMinValue) / height + offsetY),
+                        new Point((lineX + 1) * stepX / 2.0, stepY * (nowValueMin - channelMinValue) / height + offsetY)
+                    );
+                    dc.DrawLine(
+                        new Pen(Brushes.Black, 1.0),
+                        new Point((lineX + 1) * stepX / 2.0, stepY * (nowValueMin - channelMinValue) / height + offsetY),
+                        new Point((lineX + 2) * stepX / 2.0, stepY * (nowValueMax - channelMinValue) / height + offsetY)
+                    );
+                    lineX += 2;
+
+                    prevValueMax = nowValueMax;
+                }
+            }
+            else
+            {
+                double prevValue = this.channel.values[this.Begin];
+                for (int i = 1; i < this.Length; i++)
+                {
+                    double nowValue = this.channel.values[this.Begin + i];
+                    dc.DrawLine(
+                        new Pen(Brushes.Black, 1.0),
+                        new Point((i - 1) * stepX, stepY * (prevValue - channelMinValue) / height + offsetY),
+                        new Point(i * stepX, stepY * (nowValue - channelMinValue) / height + offsetY)
+                    );
+                    prevValue = nowValue;
+                }
             }
         }
 
