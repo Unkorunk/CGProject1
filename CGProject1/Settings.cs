a362d70479f3717663ec436e5be14e41901ee506
Vladislav Kalinin <unkorunk@gmail.com>
2020-07-10 17:02:49 +1000

    Update Settings.

--- CGProject1/Settings.cs
+++ CGProject1/Settings.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Text;
+using System.Collections.Generic;
 
 namespace CGProject1
 {
     public class Settings
     {
         private const string Filename = "settings.ini";
-        private readonly Dictionary<string, string> Values = new Dictionary<string, string>();
+        private readonly Dictionary<string, string> values = new Dictionary<string, string>();
 
         public static Settings Instance { get; }
 
         private Settings()
         {
             if (!File.Exists(Filename)) return;
-            
             var lines = File.ReadAllLines(Filename);
+
             foreach (var line in lines)
             {
                 var parts = line.Split("=");
-                if (parts.Length != 2 || !parts[0].All(char.IsLower) ||
-                    parts[0].Length == 0 || parts[1].Length == 0)
+
+                if (parts.Length != 2 || !ValidateKey(parts[0]) || string.IsNullOrEmpty(parts[1]))
                 {
-                    Values.Clear();
+                    values.Clear();
                     return;
                 }
-                Values.Add(parts[0], parts[1]);
+
+                values.Add(parts[0], parts[1]);
             }
         }
-        
+
         static Settings()
         {
             Instance = new Settings();
@@ -38,35 +38,38 @@ namespace CGProject1
 
         public void Set<T>(string key, T value)
         {
-            var converted = value.ToString();
+            if (!ValidateKey(key)) return;
 
-            key = key.ToLower();
-            if (key.Length == 0 || !key.All(char.IsLower) ||
-                converted == null || converted.Length == 0) return;
+            var valueStr = value.ToString();
+            if (string.IsNullOrEmpty(valueStr)) return;
 
-            if (Values.ContainsKey(key))
+            if (values.ContainsKey(key))
             {
-                Values[key] = converted;
+                values[key] = valueStr;
             }
             else
             {
-                Values.Add(key, converted);
+                values.Add(key, valueStr);
             }
+        }
+
+        private static bool ValidateKey(string key)
+        {
+            return key.Length != 0 && key.All(x => char.IsLower(x) || char.IsUpper(x) || char.IsDigit(x) || x == '_') &&
+                   !char.IsDigit(key[0]);
         }
 
         public string Get(string key)
         {
-            key = key.ToLower();
-            if (key.Length == 0 || !key.All(char.IsLower)) return null;
-            
-            return Values.TryGetValue(key, out var value) ? value : null;
+            if (!ValidateKey(key)) return null;
+            return values.TryGetValue(key, out var value) ? value : null;
         }
 
         public void Save()
         {
             var stringBuilder = new StringBuilder();
-            
-            foreach (var (key, value) in Values)
+
+            foreach (var (key, value) in values)
             {
                 stringBuilder.AppendLine($"{key}={value}");
             }
